21c9695b5275715318f81a142263cf745db15e0d
manihp84 <maniram3327cr@gmail.com>
2024-07-14 12:22:58 +0530

    static update

--- fish/settings.py
+++ fish/settings.py
@@ -126,6 +126,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Additional locations of static files
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'waterapp/static'),
